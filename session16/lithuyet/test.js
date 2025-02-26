770e986560a9583e390cb54a76cc6c4b3617c5b4
Hungmot <hung13022507@gmail.com>
2025-02-26 16:41:05 +0700

    push code

---
+++ session16/lithuyet/test.js
@@ -0,0 +1,7 @@
+let math = prompt(" mời bạn nhập điểm toán ");
+let physics = prompt("mời bạn nhập diểm lí");
+let chemist = ("mời bạn nhập điểm hoá");
+let total = ( math + physics + chemistry)/3;
+let result= average.toFixed(2);
+console.log
+
