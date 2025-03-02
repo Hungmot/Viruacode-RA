dddda9194dcb5d420bdadbb521507dc6ae440402
Hungmot <hung13022507@gmail.com>
2025-03-03 00:08:38 +0700

    push code

---
+++ Session18/baitap/bai4.js
@@ -0,0 +1,35 @@
+let num;
+
+num = +prompt("mời bạn nhập số (0 -> 9):");
+switch (num) {
+  case 1:
+    document.write("1");
+    break;
+  case 2:
+    document.write("2");
+    break;
+  case 3:
+    document.write("3");
+    break;
+  case 4:
+    document.write("4");
+    break;
+  case 5:
+    document.write("5");
+    break;
+  case 6:
+    document.write("6");
+    break;
+  case 7:
+    document.write("7");
+    break;
+  case 8:
+    document.write("8");
+    break;
+  case 9:
+    document.write("9");
+    break;
+  default:
+    document.write("mời bạn nhập lại");
+    break;
+}
